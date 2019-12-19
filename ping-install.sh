bec6e14a3b62daa053e2b3f486ec3c6e32765e78
Natan Bensoussan <natanb@tikalk.com>
2019-12-19 17:07:16 +0200

    Add pv and pvc to the installer

--- ping-install.sh
+++ ping-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create -f svc-redis.yml -f svc-flask.yml -f deploy-redis.yml -f deploy-flask.yml
+kubectl create -f svc-redis.yml -f svc-flask.yml -f pv.yml -f pvc.yml -f deploy-redis.yml -f deploy-flask.yml
 echo
 echo
 kubectl get all -o wide
